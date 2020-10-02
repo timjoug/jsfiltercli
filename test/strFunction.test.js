27040913868f4a302695a6462473bbb5ee926e88
timjoug <tjouglet@partner.auchan.fr>
2020-10-02 15:20:28 +0200

    Step 2 : Javascript Command Line for Basic Mock string

--- test/strFunction.test.js
+++ test/strFunction.test.js
@@ -1,25 +1,35 @@
 const app = require('./../app')
 
+// Function strFilter(filterStr, checkStr)
 test('If the string to check and the filter are equal, it returns true', () => {
     const input = { filterStr: 'test', checkStr: 'test' }
 
     expect(app.strFilter(input.filterStr, input.checkStr)).toBe(true);
 });
-
 test('If the string to check includes the filter, it returns true', () => {
     const input = { filterStr: 'es', checkStr: 'test' }
 
     expect(app.strFilter(input.filterStr, input.checkStr)).toBe(true);
 });
-
 test('If the string to check does not include the filter, it returns false', () => {
     const input = { filterStr: 'h', checkStr: 'test' }
 
     expect(app.strFilter(input.filterStr, input.checkStr)).toBe(false);
 });
-
 test('If the check and/or the filter value set is not a string, the variables will be convert into string and the function will be executed', () => {
     const input = { filterStr: 27, checkStr: 27 }
 
     expect(app.strFilter(input.filterStr, input.checkStr)).toBe(true);
+});
+
+// Function filteredResult()
+test('If the filter in entry of the function works, the function will return the mock string ', () => {
+    const input = 'test'
+
+    expect(app.filteredResult(input)).toBe('Onestringtotestwithaoneliner');
+});
+test('If the filter in entry of the function doesn t work, the function will return a an empty string', () => {
+    const input = 'tset'
+
+    expect(app.filteredResult(input)).toBe('');
 });
